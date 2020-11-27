25733d478b79e7d83d400e0c731af2dc8f680de8
Rohit Dalvi <rohit.dalvi2001@gmail.com>
2020-11-27 18:43:02 +0530

    create skeleton for website

---
+++ src/components/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App() {
+  return <div className="App">Let's Start</div>;
+}
+
+export default App;
---
+++ src/components/Team.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Team() {
+  return <div>This is Team page</div>;
+}
+
+export default Team;
