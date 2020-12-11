0888f0d271f910e9249dc7cae3118e9e844887de
Maitreyi88 <maitreyi.joshi882001@gmail.com>
2020-12-11 19:07:29 +0530

    media_icons_contact_info_updated

--- src/components/ContactUsMiddle.js
+++ src/components/ContactUsMiddle.js
@@ -14,15 +14,17 @@ function AppMiddle() {
         </div>
         <div style={{ display: "flex", alignItems: "center" }}>
           <PhoneIcon style={{ fontSize: 40, color: "green" }}></PhoneIcon>
-          <h3 style={{ marginLeft: 10 }}>+91 1111111111</h3>
+          <h3 style={{ marginLeft: 10 }}>+91 9049600422</h3>
         </div>
 
         <div style={{ display: "flex", alignItems: "center" }}>
           <LocationOnIcon
             style={{ fontSize: 40, color: "red" }}
           ></LocationOnIcon>
-          <h3 style={{ marginLeft: 10 }}>Locate Us</h3>
-        </div>
+          <h3 style={{ marginLeft: 10 }}> ACES | PVG’s College of Engineering and Technology<br></br>
+          44, Vidya Nagari, Shivdarshan, Parvati,Pune – 411009 Maharashtra – INDIA</h3>
+          <br></br>
+        </div> 
       </div>
       <div></div>
     </div>
