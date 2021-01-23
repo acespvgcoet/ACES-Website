72e9317bca92fc278aaee044ef427a0e577b86af
Maitreyi88 <maitreyi.joshi882001@gmail.com>
2021-01-23 09:37:23 +0530

    1stpgEventsUI

---
+++ src/components/Events.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import EventDetails from "./EventDetails";
+import EventsPage from "./EventsPage";
+
+function Events() {
+  return (
+ <div>
+ <EventsPage></EventsPage>
+ {/* <EventDetails></EventDetails> */}
+ </div>
+  );
+}
+
+export default Events;
--- src/components/EventsPage.js
+++ src/components/EventsPage.js
@@ -1,10 +1,105 @@
 import React from 'react'
+import "../css/Events.css";
+
+import Android from "../assets/images/Android.png";
+
+import EventsCard from"./EventsCard";
+import "../css/EventsCard.css";
+// import { Android } from '@material-ui/icons';
 
 function EventsPage() {
     return (
-        <div>
-            <h1>EVENTS PAGE</h1>
+    <div>
+        <div className="Events-container">
+            <h2> ACES&nbsp; EVENTS &nbsp;BAR </h2>
+            <div className="timeline-container">
+            <div id="timeline">
+            <div className="card-image" >
+            <a
+                  href="https://www.instagram.com/acespvg/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img
+                    className="card-image"
+                    src={Android}
+                    alt="instagram-icon"
+                  />
+                </a>
+            </div>
+            <div class="card"><EventsCard 
+            title="Android Workshop"
+            day="FRI"
+            month="Jan"
+            date="23"
+            year="2020"
+            time="7"></EventsCard></div>
+            <div className="card-image2" >
+            <a
+                  href="https://www.instagram.com/acespvg/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img
+                    className="card-image2"
+                    src={Android}
+                    alt="instagram-icon"
+                  />
+                </a>
+            </div>
+            <div class="card2"><EventsCard 
+            title="Android Workshop"
+            day="FRI"
+            month="Jan"
+            date="23"
+            year="2020"
+            time="7"></EventsCard></div>
+             <div className="card-image" >
+            <a
+                  href="https://www.instagram.com/acespvg/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img
+                    className="card-image"
+                    src={Android}
+                    alt="instagram-icon"
+                  />
+                </a>
+            </div>
+            <div class="card"><EventsCard 
+            title="Android Workshop"
+            day="FRI"
+            month="Jan"
+            date="23"
+            year="2020"
+            time="7"></EventsCard></div>
+             <div className="card-image2" >
+            <a
+                  href="https://www.instagram.com/acespvg/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img
+                    className="card-image2"
+                    src={Android}
+                    alt="instagram-icon"
+                  />
+                </a>
+            </div>
+            <div class="card2"><EventsCard 
+            title="Android Workshop"
+            day="FRI"
+            month="Jan"
+            date="23"
+            year="2020"
+            time="7"></EventsCard></div>
+            </div>
+            
+            </div>   
         </div>
+    </div>
+      
     )
 }
 
