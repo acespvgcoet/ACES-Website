958f53d76abdfacc4da9641c15a3b5137dad0fdd
Maitreyi88 <maitreyi.joshi882001@gmail.com>
2021-10-16 22:46:24 +0530

    header updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,22 +5,24 @@ function Header() {
   return (
     <div className="header">
       <label for="hamburger">&#9776;</label>
-  <input type="checkbox" id="hamburger"/>
-      <NavLink className="headerLink" exact={true} to="/">
-        <p className = "headerNav">HOME</p>
-      </NavLink>
-      <NavLink className="headerLink" to="/teams">
-        <p className = "headerNav">TEAM</p>
-      </NavLink>
-      <NavLink className="headerLink" to="/blogs">
-        <p className = "headerNav">BLOGS</p>
-      </NavLink>
-      {/* <NavLink className="headerLink" to="/events">
+      <input type="checkbox" id="hamburger" />
+      <div className="NavMenu">
+        <NavLink className="headerLink" exact={true} to="/">
+          <p className="headerNav">HOME</p>
+        </NavLink>
+        <NavLink className="headerLink" to="/teams">
+          <p className="headerNav">TEAM</p>
+        </NavLink>
+        <NavLink className="headerLink" to="/blogs">
+          <p className="headerNav">BLOGS</p>
+        </NavLink>
+        {/* <NavLink className="headerLink" to="/events">
         <p className = "headerNav">EVENTS</p>
       </NavLink> */}
-      <NavLink className="headerLink" to="/contact">
-        <p className = "headerNav">CONTACT</p>
-      </NavLink>
+        <NavLink className="headerLink" to="/contact">
+          <p className="headerNav">CONTACT</p>
+        </NavLink>
+      </div>
     </div>
   );
 }
