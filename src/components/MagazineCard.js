a08c2ea8c4287d4879da9259586c3a579b42facc
Shyam Lokhande <lokhandeshyam2002@gmail.com>
2022-02-22 18:06:22 +0530

    Magazine Page uploaded

--- src/components/MagazineCard.js
+++ src/components/MagazineCard.js
@@ -2,17 +2,25 @@ import React, { useEffect, useState } from "react";
 
 import "../css/MagazineCard.css";
 
-export default function MagazineCard(){
+export default function MagazineCard({magazineName,magazineInfo}){
 
     return(
-        <div className="root">
-
-            <div className="card">
-            <img src="src\assets\images\Magazene1.png" alt="Magazene Image"/>
-            <div className="container">
-                <h4><b>Magazine 1</b></h4> 
-                <p>lorem epsum 10</p> 
+        <div className="root" >
+            <div className="heading">
+                <h1><b>{magazineName}</b></h1> 
             </div>
+            <div className="body">
+                <div className="card">
+                        <div className="image">
+                            <img  className="img" src={require('../assets/images/Magazene1.png').default} alt="Magazine Image"/>
+                        </div>
+
+                        <div className="container">
+                            <div className="heading2"><h2><b>{magazineName}</b></h2> </div>
+                            <p>{magazineInfo}</p> 
+                            <button type="button" id="downloadbutton">Download Now</button>
+                        </div>
+                </div>
             </div>
         </div>
     );
---
+++ src/components/MagazineIntro.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+
+import "../css/MagazineIntro.css";
+export default function MagazineIntro(){
+
+    return(
+        // <div className="root">
+        // <div className="Mainlogo">
+        //     <img src={require('../assets/images/aces.png').default} id = 'aceslogo'/>
+        // </div>
+        // <div className="BookCover">
+        //     <div className="MainHeading">
+        //     <h1 id="BookHeading">ACE THE TECH<br/><br/></h1>
+        //     <h2 id="SubHeading">IF YOU WANT TO ACE IN TECH<br/>DOWNLOAD ACE THE TECH</h2>
+        //     </div>
+        // <img src={require('../assets/images/Book cover.png').default} id = 'bookimage'/>
+        // </div>
+        // </div>
+        <div className="root">
+            <div className="Mainlogo">
+            <img src={require('../assets/images/aces.png').default} id = 'aceslogo'/>
+            </div>
+            <div className="MainHeading">
+                <div className="left-side">
+                    <h2 id="BookHeading">ACE THE TECH</h2>
+                    <h3 id="SubHeading">If You Want To Ace In Tech<br/>Download Ace The Tech</h3><br/>
+                    <a href="#download"><button type="button" id="downloadbutton">Download Now</button></a>
+                </div>
+                <div className="right-side">
+                    <img src={require('../assets/images/bookcover.png').default} id = 'bookcover'/>
+                    <div className="colorbox"></div>
+
+
+                </div>
+            </div>
+        </div>
+    )
+
+   
+}
