4230c2ff702c105ed0292f2a019e10509ea9c735
Atharva <atharvadeshpande174@gmail.com>
2022-03-09 00:26:21 +0530

    Implemented Download Magazine along with count

--- src/components/MagazineCard.js
+++ src/components/MagazineCard.js
@@ -1,9 +1,34 @@
 import React, { useEffect, useState } from "react";
-
+import { useCookies } from "react-cookie";
+import {v1} from 'uuid';
 import "../css/MagazineCard.css";
+import db,{time} from "./firebase";
 
 export default function MagazineCard({magazineName,magazineInfo}){
-
+    const [cookies, setCookie, removeCookie] = useCookies(['ace-the-tech']);
+    const [id,setId] = useState(v1());
+    removeCookie("ace-the-tech")
+    const download = () => {
+        if (cookies["ace-the-tech"])
+        {
+            
+            db.collection("magazine").doc(cookies["ace-the-tech"])?.collection("count").add({
+                "time": time, 
+            })
+        }
+        else
+        {
+            
+            
+            setCookie("ace-the-tech",id)
+            db.collection("magazine").doc(id).set(
+                {
+                    "time": time
+                }
+            )
+        }
+        window.location.href = "https://firebasestorage.googleapis.com/v0/b/aces-website-9fc34.appspot.com/o/ACES.pdf?alt=media&token=23c96b78-2da5-4072-a960-a52921fabd91"
+    }
     return(
         <div className="root" >
             <div className="heading">
@@ -18,7 +43,7 @@ export default function MagazineCard({magazineName,magazineInfo}){
                         <div className="container">
                             <div className="heading2"><h2><b>{magazineName}</b></h2> </div>
                             <p>{magazineInfo}</p> 
-                            <button type="button" id="downloadbutton">Download Now</button>
+                            <button type="button" id="downloadbutton" onClick={download}>Download Now</button>
                         </div>
                 </div>
             </div>
--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import App from "./components/App";
 import "leaflet/dist/leaflet.css";
 import reducer, { initialState } from "./components/reducer";
 import  {StateProvider} from './components/StateProvider'
+import { CookiesProvider } from "react-cookie";
 ReactDOM.render(
     <React.StrictMode>
         <StateProvider initialState={initialState} reducer={reducer}>
-            <App />
+            <CookiesProvider>
+                <App />
+            </CookiesProvider>
         </StateProvider>
     </React.StrictMode>,
     document.getElementById("root")
